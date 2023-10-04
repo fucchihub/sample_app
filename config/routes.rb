7dde4d7ddebee89944c9de3440df1cecab586973
fucchihub <144200649+fucchihub@users.noreply.github.com>
2023-10-04 14:47:13 +0000

    [Finish]3-1

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  get 'lists/new'
+  get 'lists/index'
+  get 'lists/show'
+  get 'lists/edit'
+  get '/top' => 'homes#top'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
+
+# get '/top' => 'homes#top' だと～～～.com/top のようにパスが1個になり、
+#/topにアクセスしたときにtopアクションが実行されるようになる。
+
+# get 'homes/top' => 'homes#top' を省略して get 'homes/top' と書くことができる。
+# 　　　　URL　　と　'コントローラ名#アクション名' の形が全く同じになる場合は、
+#                    'コントローラ名#アクション名'の部分を省略することができる。
+# ～～～.com/homes/top とパスが増える。
