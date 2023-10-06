a38b68a6e556864d957d839506279ffa8659714c
fucchihub <144200649+fucchihub@users.noreply.github.com>
2023-10-06 17:14:39 +0000

    [Finish]3-4

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,13 +4,9 @@ class ListsController < ApplicationController
     @list = List.new
   end
 
-# 以下を追加
   def create
-    # １.&2. データを受け取り新規登録するためのインスタンス作成
     list = List.new(list_params)
-    # 3. データをデータベースに保存するためのsaveメソッド実行
     list.save
-    # 4. トップ画面へリダイレクト
     redirect_to list_path(list.id)
   end
 
@@ -19,9 +15,11 @@ class ListsController < ApplicationController
   end
 
   def show
+    @list = List.find(params[:id])
   end
 
   def edit
+    @list = List.find(params[:id])
   end
 
   private
